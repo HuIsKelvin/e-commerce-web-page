b140d01ad9fb0f3a193b41eb5ea064a3c88ed854
hurk <huiskelvin@qq.com>
2019-03-30 10:34:49 +0800

    fix the category hover problem

--- src/asset/js/home/topNav.js
+++ src/asset/js/home/topNav.js
@@ -3,20 +3,34 @@
 let cart = document.getElementsByClassName("topbar-cart")[0];
 let cart_menu = document.getElementsByClassName("cart-menu")[0];
 
-cart.addEventListener("mouseenter", () => {
+// show the content in cart
+cart.addEventListener("mouseenter", function() {
   cart_menu.style.display = "block";
 })
-cart.addEventListener("mouseleave", () => {
+cart.addEventListener("mouseleave", function() {
   cart_menu.style.display = "none";
 })
 
 // --- header ---
 // --- search ---
 let form_search = document.getElementById("form-search");
-form_search.addEventListener("focusin", (e)=> {
+form_search.addEventListener("focusin", function(e) {
   let color = "#FF6700";
   e.target.style.borderColor = color;
 })
-form_search.addEventListener("focusout", (e)=> {
+form_search.addEventListener("focusout", function(e) {
   e.target.style.borderColor = "#e0e0e0";
 })
+
+// --- hero ---
+// let category_list = document.querySelector(".home-hero .home-hero-category");
+// category_list.addEventListener("mouseover", function(e) {
+//   if(e.target.className === "list-item" || e.target.className === "category-list-children") {
+//     document.getElementById("list-children").style.display = "block";
+//   }
+// })
+// category_list.addEventListener("mouseout", function(e) {
+//   if(e.target.className === "list-item" || e.target.className === "category-list-children") {
+//     document.getElementById("list-children").style.display = "none";
+//   }
+// })
